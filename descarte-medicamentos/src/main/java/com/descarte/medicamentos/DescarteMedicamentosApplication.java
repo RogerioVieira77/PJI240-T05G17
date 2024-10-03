031a6d90cf73958b551cf511318e20a117c03101
RogerioVieira77 <rogerio.vieira77@hotmail.com>
2024-10-03 18:00:20 -0300

    Importação do Projeto no Eclipse

---
+++ descarte-medicamentos/src/main/java/com/descarte/medicamentos/DescarteMedicamentosApplication.java
@@ -0,0 +1,13 @@
+package com.descarte.medicamentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DescarteMedicamentosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DescarteMedicamentosApplication.class, args);
+	}
+
+}
---
+++ descarte-medicamentos/src/test/java/com/descarte/medicamentos/DescarteMedicamentosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.descarte.medicamentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DescarteMedicamentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
