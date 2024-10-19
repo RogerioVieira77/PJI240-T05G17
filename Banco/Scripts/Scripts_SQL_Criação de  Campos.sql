5c9f15979f4060f5fbf01d060cda6c7dfb8686bf
RogerioVieira77 <rogerio.vieira77@hotmail.com>
2024-10-19 07:24:12 -0300

    Campo Zona e Região funcionando

--- Banco/Scripts/Scripts_SQL_Criação de  Campos.sql
+++ Banco/Scripts/Scripts_SQL_Criação de  Campos.sql
@@ -3,4 +3,4 @@ alter table ponto_coleta add column regiao_cod int not null;
 
 /* Alteração de Campo */
 /* Dropa e Cria de novo */
-alter table tabela_teste drop column campo_teste;
+alter table ponto_coleta drop column regiao;
